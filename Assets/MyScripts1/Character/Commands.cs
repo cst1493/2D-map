205008b50842ddaf122970927d7e872c90f0bc6c
Cameron <cst1493@gmail.com>
2020-08-07 02:06:46 -0500

    Setup character States and moved files around.
    temp solution of a state machine.

---
+++ Assets/MyScripts1/Character/Commands.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Commands : MonoBehaviour
+{
+    public static readonly string allyParentName = "Allies"; //name of empty parent gameObject of all player characters.
+    private float unblockTime = 0.5f;
+    private bool blockSelectingChars = false;
+
+
+    private Vector2 targetPos, currentPos;
+    private bool objectsSelectable;
+    private string playerCommand;
+    private bool charSelected;
+
+
+    private GameObject selectedGameObject;
+
+    void Start()
+    {
+        objectsSelectable = true;
+        charSelected = false;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (objectsSelectable) //attempt to select gameObject
+                selectedGameObject = RayHit();
+            else if (charSelected)
+            {
+                CommandCharacter();
+            }
+        }
+    }
+
+
+    private void CommandCharacter()
+    {
+        playerCommand = "move"; //temp.  playerCommand = GetPlayerCommand(); //enable UI to select command.
+
+        if (playerCommand == "move")
+        {
+            targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //mouse pos relitive to camera where 0,0 is center of camera.
+            selectedGameObject.GetComponent<States>().Move(targetPos);
+
+            DeselectCharacter();
+        }
+        else if (playerCommand == "cancel") { return; }
+
+    }
+    private GameObject RayHit()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero); //get object on mouse position
+        if ((hit.collider != null) && (objectsSelectable)) //if hit any gameObject
+        {
+            Debug.Log(hit.transform.name + " was clicked.");
+            objectsSelectable = false;
+            charSelected = true;
+            return hit.transform.gameObject;
+        }
+        return null;
+    }
+    private void DeselectCharacter()
+    {
+        playerCommand = null;
+        charSelected = false;
+        objectsSelectable = true;
+    }
+}
---
+++ Assets/MyScripts1/Character/States.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class States : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    private string currentState;
+    private Vector2 targetPos;
+    private Vector2 currentPos;
+    void Start()
+    {
+        if (speed == 0) { speed = 5.0f; }
+        targetPos = transform.position;
+    }
+
+    void Update()
+    {
+        //TODO if (collition with enemy) {(/*static*/ playerAllyFighter) = thisCharacter;}
+        if (currentState == "moving")
+            Moving();
+    }
+
+    private void Moving()
+    {
+        currentPos = transform.position; //update current position in the movement
+        transform.position = Vector2.MoveTowards(currentPos, targetPos, (speed * Time.deltaTime));
+
+        if (currentPos == targetPos) currentState = null;
+    }
+
+    public void Move(Vector2 targetPosition) //SelectCharacter.cs --> selectedGameObject.GetComponent<States>().Move(targetPos);
+    {
+        targetPos = targetPosition;
+        currentState = "moving";
+    }
+}
